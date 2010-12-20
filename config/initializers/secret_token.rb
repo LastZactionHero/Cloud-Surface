28265160a82fcf79b9727c04df51436f24292223
Zach Dicklin <zdicklin@zdicklin-laptop.(none)>
2010-12-20 14:07:58 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudsurface::Application.config.secret_token = '1f480615e37483f8ec877b4377d717fe478fc306934fd5083488c1aa638bb75ec050f42ffc288003e9ebe978bad9d8a15d0cd4c08a280c625843a46bd0b06ade'
